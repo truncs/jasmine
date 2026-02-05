135f8273c95d1b9b282514f4f3fa17ed9b3d5677
aditya <aditya@earth>
2026-02-05 10:36:30 -0800

    jnp array.

--- jasmine/models/genie.py
+++ jasmine/models/genie.py
@@ -720,7 +720,7 @@ class GenieDiffusion(nnx.Module):
         tau = [i/K for i in range(K)] + [1.0]
         tau_idx = [i*scale for i in range(K)] + [self.dyna_kmax]
         dt = 1.0 / K
-        return jax.Array(tau), jax.Array(tau_idx), dt
+        return jnp.array(tau), jnp.array(tau_idx), dt
 
     def sample(
         self,
