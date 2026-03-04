99d6af013af8b9123809abefc62fe8aa43efdcd1
aditya <aditya@earth>
2026-03-03 16:55:33 -0800

    use the appropriate return values.

--- jasmine/baselines/diffusion/train_tokenizer_mae.py
+++ jasmine/baselines/diffusion/train_tokenizer_mae.py
@@ -201,7 +201,7 @@ class Dreamer4TokenizerMAE(nnx.Module):
         B, T, H, W, C = videos.shape
         if self.is_latent_model:
             videos = videos.reshape(B*T, H, W, C)
-            patches = jax.lax.stop_gradient(self.latent_model(videos, training=False))
+            patches = jax.lax.stop_gradient(self.latent_model(videos, training=False))['x_norm_patchtokens']
             patches = patches.reshape(B, T, *patches.shape[1:])
         else:
             patches = patchify(videos, self.patch_size) # (B, T, Hp, Wp, D)
