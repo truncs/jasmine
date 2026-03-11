fe075743121a3d5853e2fa5125f55102b0a2a097
aditya <aditya@earth>
2026-03-10 18:30:58 -0700

    add treemap restore_args.

--- jasmine/models/genie.py
+++ jasmine/models/genie.py
@@ -918,12 +918,18 @@ def restore_genie_components(
     abstract_sharded_tokenizer_optimizer_state = _create_abstract_sharded_pytree(
         dummy_tokenizer_optimizer_state, sharding
     )
+
+    restore_args = jax.tree_util.tree_map(
+        lambda _: ocp.RestoreArgs(dtype=args.dtype),
+        abstract_sharded_tokenizer_optimizer_state
+    )
+
     restored_tokenizer = tokenizer_checkpoint_manager.restore(
         step=tokenizer_checkpoint_manager.latest_step(),
         args=ocp.args.Composite(
             model_state=ocp.args.PyTreeRestore(  # type: ignore
                 item=abstract_sharded_tokenizer_optimizer_state,  # type: ignore
-                restore_args=ocp.args.StandardRestoreArgs(dtype=args.dtype)
+                restore_args=restore_args
             ),
         ),
     )["model_state"]
